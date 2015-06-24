f44d30f41adc5a4dc2c62066d1a63ec0ace310b6
Thomas Duveau <thomas.duveau@hotmail.fr>
2015-06-24 20:58:59 +0200

    classe fusionvideo

---
+++ App40's/app/src/main/java/app40s/app40s/FusionVideo.java
@@ -0,0 +1,11 @@
+package app40s.app40s;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+/**
+ * Created by ThomasD on 24/06/15.
+ */
+public class FusionVideo {
+    
+}
